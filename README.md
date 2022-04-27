f6f66f328b56627ec6c579d2e672b62e6ea10c0f
Exiled1 <rudyperalta831@gmail.com>
2022-04-26 22:26:19 -0700

    Conformed with the requirements

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Bloom Filter
 
-A simple bloom filter implementation in Rust. With some fun addendums if I have the time ;)
+A simple bloom filter implementation in Rust. With some fun addendums if I have the time :3
+
+How to compile:
+
+```
+Usage: cargo run <dictionary_name> <input_file_name> <3_hash_output_file_name> <5_hash_output_file_name>
+```
+
+If you want to get a raw binary to use instead, run these commands
+
+```
+cargo build --release
+./target/release/bloom_filter <dictionary_name> <input_file_name> <3_hash_output_file_name> <5_hash_output_file_name>
+```
+
+example
+```
+cargo build --release
+./target/release/bloom_filter src/dictionary2.txt src/sample_input.txt hash3_file.txt hash5_file.txt
+```
+
--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,58 @@ mod filter;
 use filter::BloomFilter;
 use openssl::hash::MessageDigest;
 use std::error::Error;
+use std::io::Write;
 use std::{fs, io::BufRead};
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = std::env::args().skip(1);
-    let usage = "Usage: cargo run <dictionary_name> <file_name> <3 or 5 for the hash functions>";
-    let file_name = args.next().expect(usage);
-    let other_name = args.next().expect(usage);
-    let hash_num = args.next().expect(usage);
+    let usage = "Usage: cargo run <dictionary_name> <input_file_name> <3_hash_output_file_name> <5_hash_output_file_name>";
+    let dictionary = args.next().expect(usage);
+    let input_file_name = args.next().expect(usage);
+    let output_file_name_3 = args.next().expect(usage);
+    let output_file_name_5 = args.next().expect(usage);
 
-    let file = fs::File::open(file_name)?;
-    let other_file = fs::File::open(other_name)?;
+    let dictionary_file = fs::File::open(dictionary)?;
+    let input_file = fs::File::open(input_file_name)?;
 
-    let buf_reader = std::io::BufReader::new(&file);
-    let checker_buf_reader = std::io::BufReader::new(&other_file);
+    let buf_reader = std::io::BufReader::new(&dictionary_file);
+    let input_buf_reader = std::io::BufReader::new(&input_file);
 
-    let mut bloom_filter = match hash_num.parse::<u8>()? {
-        3 => BloomFilter::new(
-            vec![
-                MessageDigest::shake_256(),
-                MessageDigest::sha512(),
-                MessageDigest::sha1(),
-            ],
-            (*&file.metadata()?.len().to_owned()) as usize,
-        ),
+    let mut bloom_filter_1 = BloomFilter::new(
+        vec![
+            MessageDigest::shake_256(),
+            MessageDigest::sha512(),
+            MessageDigest::sha1(),
+        ],
+        (*&dictionary_file.metadata()?.len().to_owned()) as usize,
+    );
 
-        5 => BloomFilter::new(
-            vec![
-                MessageDigest::sha3_256(),
-                MessageDigest::shake_256(),
-                MessageDigest::sha512(),
-                MessageDigest::sha1(),
-                MessageDigest::sha384(),
-            ],
-            (*&file.metadata()?.len().to_owned()) as usize,
-        ),
-        _ => {
-            panic!("Invalid hash number, please input 3 or 5")
-        }
-    };
+    let mut bloom_filter_2 = BloomFilter::new(
+        vec![
+            MessageDigest::sha3_256(),
+            MessageDigest::shake_256(),
+            MessageDigest::sha512(),
+            MessageDigest::sha1(),
+            MessageDigest::sha384(),
+        ],
+        (*&dictionary_file.metadata()?.len().to_owned()) as usize,
+    );
 
     for line in buf_reader.lines() {
         let line = line?;
-        bloom_filter.insert(&line)?;
+        bloom_filter_1.insert(&line)?;
+        bloom_filter_2.insert(&line)?;
     }
-    for line in checker_buf_reader.lines().skip(1) {
+
+    let mut out_file_1 = fs::File::create(output_file_name_3)?;
+    let mut out_file_2 = fs::File::create(output_file_name_5)?;
+    for line in input_buf_reader.lines().skip(1) {
         let line = line?;
-        println!("{line} {}", bloom_filter.lookup(&line)?);
+        let file_1_buf = format!("{line} {}\n",bloom_filter_1.lookup(&line)?);
+        let file_2_buf = format!("{line} {}\n",bloom_filter_2.lookup(&line)?);
+        
+        out_file_1.write_all(file_1_buf.as_bytes())?;
+        out_file_2.write_all(file_2_buf.as_bytes())?;
+        
     }
 
     Ok(())
